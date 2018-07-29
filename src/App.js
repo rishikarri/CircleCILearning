664dde9df94b45ce76be2c983f98ec6eedfead0f
Rishi Karri <rishikarri1@gmail.com>
2018-07-29 18:25:26 -0400

    foo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        continuous deployment check
+        continuous deployment check number 3
       </div>
     );
   }
